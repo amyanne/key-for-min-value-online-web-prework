7692ed7f443aa992ca1bdb375b8f9c4cef1a9436
Amy Westcott <amy.westcott@yahoo.com>
2019-06-06 21:45:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require "pry"
 def key_for_min_value(name_hash)
   low_name, low_num = name_hash.first
   name_hash.each do |name, num|
-    current_name, current_age = name_hash[name]
+    current_age= name_hash[name]
    binding.pry
     if current_age < low_num
        low_num = current_age
