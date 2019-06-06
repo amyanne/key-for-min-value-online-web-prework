3ed3634037224b62e5e2cc1d30614765ae255bcb
Amy Westcott <amy.westcott@yahoo.com>
2019-06-06 21:57:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   low_name, low_num = name_hash.first
   name_hash.each do |name, num|
    
-    if num < low_num
+    if name && num < low_num
        low_num = name_hash[name]
        binding.pry
     end
