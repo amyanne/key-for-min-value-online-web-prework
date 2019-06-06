e2267f675caf2b2586c0bb3877c3ae50874362be
Amy Westcott <amy.westcott@yahoo.com>
2019-06-06 21:43:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   low_name, low_num = name_hash.first
   name_hash.each do |name, num|
     current_name, current_age = name_hash[name]
-    if current < low_num
+    if current_age < low_num
        low_num = current
       
     end
